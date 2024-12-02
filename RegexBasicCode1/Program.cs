95e922f9fd188c32599a95b5c5d8572eb28032de
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-03 00:20:23 +0100

    Add files via upload

---
+++ RegexBasicCode1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FirstProject
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^([a-z0-9]+)\.?([a-z0-9]+)@([a-z]+)\.[a-z]{2,3}$");
+            string email = "testm.test2@gmail.com";
+
+            bool isMatch = regex.IsMatch(email);
+            Console.WriteLine(isMatch);
+        }
+    }
+}
